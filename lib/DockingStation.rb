1e271e4343302d135c48ceeee7e77cf3b2503a5e
marvinmatovu@gmail.com <student@Makerss-MacBook-Pro.local>
2019-06-26 17:14:12 +0100

    step 14 boris bikes

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,23 @@
 class DockingStation
-  def release_bike
+  def initialize
+    @the_dock = []
+  end
 
+  def release_bike
+    fail 'No bikes available' unless @bike
+  end
+
+  def dock(bike)
+    if @the_dock.count < 20
+      @the_dock << bike
+    else
+      raise 'Docking full!'
+    end
+  end
+
+  def bike
+    raise 'No bikes available' unless @bike
+    @bike
   end
 end
 
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,3 +3,40 @@ require 'DockingStation'
 describe DockingStation do
   it { should respond_to(:release_bike) }
 end
+
+describe DockingStation do
+  it { should respond_to(:dock)}
+end
+
+describe DockingStation do
+  it {should respond_to(:bike)}
+end
+
+describe DockingStation do
+  describe '#bike' do
+    it 'raises' do
+      station = DockingStation.new
+      expect {station.bike}.to raise_error 'No bikes available'
+    end
+  end
+end
+
+describe DockingStation do
+  describe '#dock' do
+    it 'raises' do
+      station = DockingStation.new
+      bike = Bike.new
+      expect {station.dock(bike)}.to raise_error 'Docking full!'
+    end
+  end
+end
+
+describe DockingStation do
+  describe '#dock' do
+    it 'raises' do
+      station = DockingStation.new
+      20.times {station.dock(Bike.new)}
+      expect { station.dock(Bike.new)}.to raise_error 'Docking full!'
+    end
+  end
+end
